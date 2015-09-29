80470e9e546b0d72355ec00c78d108d3cf2093ba
Ralph Khattar <ralph.khattar@gmail.com>
2015-09-29 14:53:04 -0400

    Removed unneeded memory bump for tests.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,4 @@
 <?php
 
-// Because code coverage is messy
-ini_set('memory_limit','1024M');
-
 $loader = require __DIR__ . '/../vendor/autoload.php';
 $loader->setPsr4("Aol\\Offload\\Tests\\", __DIR__ . '/src');
-
